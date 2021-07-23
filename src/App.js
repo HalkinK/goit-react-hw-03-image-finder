bdbff86a3161a4518aaa191392d56198d99c1ad7
Konstantin Halkin <constantingal@gmail.com>
2021-07-23 17:33:19 +0300

    показывает Searchbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from "react";
+import { ToastContainer } from "react-toastify";
 import "./App.css";
+import Serchbar from "./components/Searchbar/Searchbar";
 
 class App extends React.Component {
+  state = { searchQuery: "" };
+
+  handleFormSubmit = (searchQuery) => {
+    this.setState({ searchQuery });
+  };
+
   render() {
-    return;
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+    return (
+      <div>
+        <Serchbar onSubmit={this.handleFormSubmit} />
+        <ToastContainer position="top-center" autoClose={3000} />
+      </div>
+    );
   }
 }
 
